919d08c753e87d75f1dcf2a44aa5ec8e74358cb6
eemim <eemi.mantyla@gmail.com>
2023-04-04 11:23:12 +0300

    IMPROVED FETCH

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,27 +1,31 @@
 import React, { useState, useEffect } from "react";
+import { AgGridReact } from "ag-grid-react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function QuestionList() {
-  const [questions, setQuestions] = useState([]);
+  const [survey , setSurvey] = useState([]);
 
   useEffect(() => fetchData(), []);
 
   const fetchData = () => {
-    fetch("blaablaa.com")
+    fetch("http://localhost:8080/api/surveys")
       .then((response) => response.json())
-      .then((responseData) => setQuestions('data'))
+      .then((responseData) => setSurvey(responseData._embedded.surveys))
       .catch((err) => console.error(err));
+
   };
 
   const columnDefs = [
-    {},
-    {},
-    {},
-    {}
+    {field: "name"},
     ];
 
   return (
-    <div>
-      <h1>Moro moro</h1>
+    <div className="ag-theme-material" style={{ height: 600, width: "100%", margin: "auto" }}>
+      
+      <AgGridReact
+      columnDefs={columnDefs}
+      rowData={survey}></AgGridReact>
     </div>
   );
 }
