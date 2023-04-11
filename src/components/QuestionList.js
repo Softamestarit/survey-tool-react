461a75871e47399cbf98d27f97855ba4910c454b
eemim <eemi.mantyla@gmail.com>
2023-04-11 08:17:45 +0300

    mapping fixed

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -4,40 +4,55 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 
 export default function QuestionList() {
-  const [survey , setSurvey] = useState([]);
+  const [survey, setSurvey] = useState([]);
 
   useEffect(() => fetchData(), []);
 
   const fetchData = () => {
-    fetch("http://localhost:8080/api/surveys")
+    fetch("http://localhost:8080/surveys")
       .then((response) => response.json())
-      .then((responseData) => setSurvey(responseData._embedded.surveys))
+      .then((responseData) => {
+        setSurvey(responseData);
+        console.log(responseData);
+      })
       .catch((err) => console.error(err));
-
   };
 
-  const columnDefs = [
-    {field: "name"},
-    ];
+ /* const columnDefs = [
+    { field: "name" },
+    { field: "questions[0].content", headerName: "Question" },
+  ]; */
 
   return (
-   /* <div className="ag-theme-material" style={{ height: 600, width: "100%", margin: "auto" }}>
+    /**   <div className="ag-theme-material" style={{ height: 600, width: "100%", margin: "auto" }}>
       
       <AgGridReact
       columnDefs={columnDefs}
       rowData={survey}></AgGridReact>
     </div> */
-<div>
-    <h2>SOFTAMESTARIEN KYSELYT</h2>
-<table>
-<tbody>
-<tr><th>{survey.name}</th></tr>
-{ survey.map((question) =>
-<tr key={question.id}>
-<td>{question.name}</td>
-</tr> )}
-</tbody>
-</table>
-</div>
+
+    <div>
+      <h1>Softamestarien superkysely</h1>
+      <table>
+        <thead>
+          <tr>
+            {survey.map((survey, index) => (
+              <th key={index}>{survey.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {survey.map((survey, index) => (
+              <td key={index}>
+                {survey.questions.map((question, index) => (
+                  <p key={index}>{question.content}</p>
+                ))}
+              </td>
+            ))}
+          </tr>
+        </tbody>
+      </table>
+    </div>
   );
 }
