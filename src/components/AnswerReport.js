1518f34ac2543682dc4e6917f49ff8155e181bc4
mededaniela <mededaniela99@gmail.com>
2023-04-27 09:42:03 +0300

    Edited the AnswerReport style

--- src/components/AnswerReport.js
+++ src/components/AnswerReport.js
@@ -1,7 +1,11 @@
 import React, { useState, useEffect} from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 
 export default function AnswerReport() {
     const [questions, setQuestions] = useState([]);
+    const [title, setTitle] = useState('');
     
     useEffect (() => fetchData(), []);
 
@@ -9,21 +13,29 @@ export default function AnswerReport() {
         fetch("http://localhost:8080/surveys/1")
           .then((response) => response.json())
           .then((responseData) => {
-            setQuestions(responseData.questions);
+            setQuestions(responseData.questions)
+            setTitle(responseData.title);
           })
           .catch((err) => console.error(err));
     };
 
     console.log(questions)
 
+    const columns = [
+        { headerName: "Question", field: "questions" },
+		{ headerName: "Answer", field: "answers" },
+		
+    ]
+
     return (
         <div>
-            <h1>Kyselyn vastaukset {}</h1>
+            <h1>Kyselyn vastaukset</h1>
+            <h2>{title}</h2>
 
             <ul>
-                {questions.map((question, questionId) => (
+               {questions.map((question, questionId) => (
                 <li key={questionId}>
-                    <p>{question.content}</p>
+                    <b> <h3>{question.content}</h3></b>
                 
                 {question.answers.map((answer, answerId) => (
                     <p key={answerId}>
