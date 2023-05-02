eda429d28ffd70c419900d5b2eae77875d4c49dc
Olli Myyryläinen <myyrylainen.olli@gmail.com>
2023-05-02 09:28:41 +0300

    User can now answer to survey id 1. Rename QuestionsAndAnswers to SurveyResponseForm. Add answers module for saving answers to the backend.

---
+++ src/modules/answers.js
@@ -0,0 +1,14 @@
+export const saveAnswers = (answers) => {
+
+    // https://survey-tool-spring-production.up.railway.app/answers
+    return fetch(`https://survey-tool-spring-production.up.railway.app/answers`, {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(Object.values(answers))
+    })
+        .then(response => response.json())
+        .catch(error => console.error(error));
+      
+};
