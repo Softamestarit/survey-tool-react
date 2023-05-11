48281e8414d4e0809cdf3b8595c322ffa818735e
mededaniela <mededaniela99@gmail.com>
2023-05-11 10:39:48 +0300

    Added appbar and logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
 import './App.css';
 import SurveyResponseForm from './components/SurveyResponseForm';
 import AnswerReport from './components/AnswerReport';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import logo from './components/logo.png';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <AppBar position="static" style={{ height: '100px' }}>
+          <Toolbar>
+          <div style={{ padding: '16px' }}>
+            <img src={logo} alt="Logo" width="220" height="70" />
+            </div>
+          </Toolbar>
+        </AppBar>
         <Routes>
           <Route path="/survey/:id/response" element={<SurveyResponseForm />} />
           <Route path="/survey/:id/report" element={<AnswerReport />} />
@@ -15,6 +25,5 @@ function App() {
     </div>
   );
 }
-
 
 export default App;
